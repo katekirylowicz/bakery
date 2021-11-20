ec24c0eef13ba489977b742620a8ce990d3e8b2d
Katarzyna Kiryłowicz <k.kirylowicz@gmail.com>
2021-11-20 14:06:29 +0100

    Initial bakery home page.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import Home from "./pages/home";
+
+import "./scss/main.scss";
+import Footer from "./common/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+
+      </Routes>
+      <Footer />
+
+    </BrowserRouter>
   );
 }
 
