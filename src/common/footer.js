1559e17eb89243173a5bffe73df805607c6ac597
Katarzyna Kiryłowicz <k.kirylowicz@gmail.com>
2021-11-30 22:17:36 +0100

    Add e-mail to footer section.

--- src/common/footer.js
+++ src/common/footer.js
@@ -6,12 +6,21 @@ const Footer = () => {
     <>
       <footer>
         <div className="foo-container">
-
-          <div>Contact with us : <span><img className="mobi_ico"
-            src="/assets/mobile-solid.svg"
-            alt="mobile-ico" /></span> <a className="contact_num" href="tel:+48666666666"> 666 666 666</a> </div>
+<div>
+          <div className="contact_section">Contact with us :
+            
+              <span><img className="mobi_ico"
+                src="/assets/mobile-solid.svg"
+                alt="mobile-ico" /></span>
+              <a className="contact_num" href="tel:+48666666666"> 666 666 666</a>
+            </div>
+            <div>
+              <span><img className="e-mail_ico"
+                src="/assets/at-solid.svg"
+                alt="at-ico" /></span> <a className="contact_mail" href="mailto:artofbread@gmail.com?subject=tytuł listu">artofbread@gmail.com</a>
+            </div>
+          </div>
           <p>&copy;2021 Kiryłowicz</p>
-
         </div>
       </footer>
     </>
